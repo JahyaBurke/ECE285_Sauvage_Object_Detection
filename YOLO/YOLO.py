2bb6529c8c1de067daefe722697a78eaa21dbab4
samondal <samondal@samondal-12685.pods.samondal.svc.cluster.local>
2019-06-09 19:28:51 -0700

    YOLO_mAP

--- YOLO/YOLO.py
+++ YOLO/YOLO.py
@@ -222,8 +222,6 @@ class Darknet(nn.Module):
                 anchors = self.module_list[i][0].anchors
                 #Get the input dimensions
                 inp_dim = int (self.net_info["height"])
-                
-                print(inp_dim)
         
                 #Get the number of classes
                 num_classes = int (module["classes"])
